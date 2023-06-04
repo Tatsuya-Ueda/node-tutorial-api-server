d741e327f9ed307d3a5118087294ffc913aef2cd
Tatsuya-Ueda <74095322+Tatsuya-Ueda@users.noreply.github.com>
2023-06-04 13:22:46 +0900

    dayjsをlogmiddlewareの日付表示に使ってみた

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const dayjs = require("dayjs");
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.status(200).send("hello!");
+// HTTPメソッドとリクエストのURL(root以下の文字列)をconsoleに出力するミドルウェア
+const logMiddleWare = (req, res, next) => {
+  const now = dayjs();
+  console.log(now["$d"], req.method, req.url);
+  next();
+};
+
+app.use(logMiddleWare);
+
+app.get("/", (req, res, next) => {
+  res.status(200).send("hello!\n");
 });
 
 app.get("/user/:id", (req, res) => {
